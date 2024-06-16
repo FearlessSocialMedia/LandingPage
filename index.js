a9c670d75993664a675a41a62f0ec543c3cdb7fb
Shivam Pande <shivshivampande18@gmail.com>
2024-06-16 23:07:31 +0530

    Files added

---
+++ index.js
@@ -0,0 +1,6 @@
+// var genChild = document.getElementsByClassName('genChild');
+
+// function generate(){
+//     genChild[0].innerHTML = "Play Rock Paper Scissors Lizard Spock with a Stranger";
+//     genChild[1].style.display = "none";
+// }
